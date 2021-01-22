ca7bc9abc82c635f3149bcabfc457d4836952d67
zhadjah9559 <zhadjah@gmail.com>
2021-01-22 11:09:25 -0500

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarPro.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        //validation annotation
+        public int ModelYear { get; set; }
+
+        public int Mileage { get; set; }
+
+        [DisplayName("Price")]
+        [RegularExpression(@"^\$?\d+(\.(\d{2}))?$")]
+        public Decimal Price { get; set; }
+
+        public string Color { get; set; }
+
+        //find annotation to properly display that
+        public string WheelDrive { get; set; }
+
+
+
+        //How to declare a Parent (Foreign Key)
+        public int LotId { get; set; }
+        public virtual Lot Lot { get; set; }
+    }
+}
---
+++ Models/Lot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarPro.Models
+{
+    public class Lot
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+        public int ZipCode { get; set; }
+
+        [Display(Name = "(123) 456-7890")]
+        public string PhoneNumber  { get; set; }
+
+        [Display(Name = "Manager Name")]
+        public string ManagerName { get; set; }
+
+
+
+    }
+}
